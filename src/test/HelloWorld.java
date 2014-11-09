b7bca3e6060f99573621348e6a36d2c837b7bc69
dstammb13 <dominik.stammbach@uzh.ch>
2014-11-09 19:56:17 +0100

    test

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package test;
+
+public class HelloWorld {
+	public void printHelloWorld() {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld o1 = new HelloWorld();
+		o1.printHelloWorld();
+	}
+}
+
+
